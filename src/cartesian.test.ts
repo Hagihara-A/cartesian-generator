cee6cee8756042fba06add230b9995371b56575f
Arark <knkr5456@gmail.com>
2021-03-03 14:34:53 +0900

    test: testcase that asserts product of string | number | object

--- src/cartesian.test.ts
+++ src/cartesian.test.ts
@@ -32,3 +32,22 @@ test("...[] => []", () => {
   expect(prod.next().value).toEqual([]);
   expect(prod.next().done).toBeTruthy();
 });
+
+test(`["1", "2"], ["3", "4"] => ["1", "3"], ["1", "4"], ["2", "3"], ["2","4"]`, () => {
+  const prod = cartesian(["1", "2"], ["3", "4"]);
+  expect(prod.next().value).toEqual(["1", "3"]);
+  expect(prod.next().value).toEqual(["1", "4"]);
+  expect(prod.next().value).toEqual(["2", "3"]);
+  expect(prod.next().value).toEqual(["2", "4"]);
+});
+
+test(`[{}, "1"], ["2", {a:""}, 3] => [{}, "2"]@0, ["1", {a:""}]@4, ["1", 3]@5`, () => {
+  const prod = cartesian([{}, "1"], ["2", { a: "" }, 3]);
+  expect.assertions(3);
+  for (let i = 0; i < 6; i++) {
+    const curr = prod.next();
+    if (i === 0) expect(curr.value).toEqual([{}, "2"]);
+    else if (i == 4) expect(curr.value).toEqual(["1", { a: "" }]);
+    else if (i == 5) expect(curr.value).toEqual(["1", 3]);
+  }
+});
